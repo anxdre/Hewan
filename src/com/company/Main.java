4d575ae83bb6e32e597849d8ca3e00dfd9f208c9
Andre Setiawan A <ahmadiandre@gmail.com>
2019-09-30 20:50:15 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import model.Beruang;
+import model.Kucing;
+
+public class Main {
+    public static void main(String[] args) {
+        Kucing wenny = new Kucing("wenny", 4, "meow", "putih");
+        wenny.displayMakan();
+    }
+}
